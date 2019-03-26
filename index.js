9e9b4b6d09ad94dc9d0ad0948b546734cf72e4e2
yuting6304 <s5333815@yahoo.com.tw>
2019-03-26 22:17:30 +0800

    Finish basic used instruction

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 var Web3 = require('web3');
 var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 
-if(!web3.eth.net.isListening()){
+if(!web3.isConnected()){
     console.log("Please start your node");
 }
 else{
+    // web3.eth.accounts.create();
+    console.log(web3.version);
     console.log("Connect successfully!");
-}
-// console.log("isConnected=");
-// web3.eth.net.isListening().then(console.log);
+    console.log('coinbase : ' + web3.eth.coinbase);
+    console.log(web3.eth.accounts);
+    let balance = web3.fromWei(web3.eth.getBalance(web3.eth.accounts[0]), 'ether').toFixed(2);
+    console.log(balance);
 
+}
 
 
 
